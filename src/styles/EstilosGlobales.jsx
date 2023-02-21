e40e95a7576a26f85d90a57aeb89b9f8932e1ceb
Diego Aldao <diego_ind@live.com.ar>
2023-02-21 13:55:26 -0300

    añadidos los archivos css iniciales

---
+++ src/styles/EstilosGlobales.jsx
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+import Variables from "./Variables";
+
+const EstilosGlobales = createGlobalStyle`
+    ${Variables};
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: var(--fuente-principal);
+    }
+    button, input{
+        background: none;
+        outline-color: transparent;
+        border: none;
+    }
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+`;
+export default EstilosGlobales;
---
+++ src/styles/Variables.jsx
@@ -0,0 +1,26 @@
+import { css } from "styled-components";
+
+const Variables = css`
+  :root {
+    --color-principal: #fed5db;
+    --color-secundario: #ffdfc0;
+    --color-promo: #aef6f7;
+    --color-promo2: #e690bd;
+    --gradiente-principal: linear-gradient(
+      to left,
+      var(--color-principal),
+      var(--color-secundario)
+    );
+    --gradiente-secundario: linear-gradient(
+      to left,
+      var(--color-promo),
+      var(--color-promo2)
+    );
+
+    --fuente-principal: "Josefin Sans", sans-serif;
+
+    --easing: cubic-bezier(0.645, 0.045, 0.355, 1);
+    --transition: all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1);
+  }
+`;
+export default Variables;
