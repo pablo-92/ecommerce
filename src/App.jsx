7f798d56e0ba13500766ed9e4894e745180c9cd7
Diego Aldao <diego_ind@live.com.ar>
2023-02-21 13:57:39 -0300

    añadidos los imports principales y la configuracion inicial de react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import PaginaPrincipal from "./pages/PaginaPrincipal";
+import EstilosGlobales from "./styles/EstilosGlobales";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PaginaPrincipal />,
+  },
+]);
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <EstilosGlobales />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
