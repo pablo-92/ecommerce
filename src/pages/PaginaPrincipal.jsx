f5ec6342d1ed02be9fb7110451f5e63d5a02345b
Diego <diego_ind@live.com.ar>
2023-03-07 11:09:38 -0300

    agregado el header

--- src/pages/PaginaPrincipal.jsx
+++ src/pages/PaginaPrincipal.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Layout from "../components/Layout";
+import styled from "styled-components";
+
+const Header = styled.div`
+    width: 100%;
+    height: 100%;
+`;
 
 const PaginaPrincipal = () => {
-  return (
-    <Layout>
-      <div>PaginaPrincipal</div>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Header></Header>
+        </Layout>
+    );
 };
 
 export default PaginaPrincipal;
